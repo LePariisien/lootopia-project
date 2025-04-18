c61c22c91d40187f41d97a79b0b618981594a2e5
Goutsse <ulyssevignolet76@gmail.com>
2025-04-18 10:19:02 +0200

    update login front interceptor

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,13 +1,12 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   private loginUrl = 'http://localhost:8080/api/auth/login';
   private verifyMfaUrl = 'http://localhost:8080/api/auth/verify-mfa';
+  private refreshUrl = 'http://localhost:8080/api/token/refresh';
 
   constructor(private http: HttpClient) {}
 
@@ -17,5 +16,13 @@ export class AuthService {
 
   verifyMfa(username: string, mfaCode: string): Observable<any> {
     return this.http.post(this.verifyMfaUrl + `?username=${username}&mfaCode=${mfaCode}`, {});
+  }
+
+  refreshToken(): Observable<any> {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (!refreshToken) {
+      return throwError(() => new Error('Pas de refreshToken'));
+    }
+    return this.http.post<any>(this.refreshUrl, { refreshToken });
   }
 }
