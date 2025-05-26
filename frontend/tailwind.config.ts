6edaf57bf0970560465321380876544655c468e7
Goutsse <ulyssevignolet76@gmail.com>
2025-05-26 16:30:25 +0200

    Changing the design of register and Login with animations

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+  animation: {
+    'fade-in': 'fadeIn 0.8s ease-out forwards',
+    'slide-up': 'slideUp 0.5s ease-out forwards',
+  },
+  keyframes: {
+    fadeIn: {
+      '0%': { opacity: '0' },
+      '100%': { opacity: '1' },
+    },
+    slideUp: {
+      '0%': { transform: 'translateY(30px)', opacity: '0' },
+      '100%': { transform: 'translateY(0)', opacity: '1' },
+    },
+  },
+},
+
+  },
+  plugins: [],
+}
+export default config
