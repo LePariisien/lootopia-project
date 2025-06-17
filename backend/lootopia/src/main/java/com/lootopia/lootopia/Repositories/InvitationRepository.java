035590fefb50401193422ebdd0dc0e4046d082e9
s2y-404 <syd95@hotmail.fr>
2025-06-17 16:32:32 +0200

    task/delete-entities-useless

--- backend/lootopia/src/main/java/com/lootopia/lootopia/Repositories/InvitationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lootopia.lootopia.Repositories;
-
-import com.lootopia.lootopia.Entities.Invitation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface InvitationRepository extends JpaRepository<Invitation, UUID> {
-}
--- backend/lootopia/src/main/java/com/lootopia/lootopia/Repositories/StepRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lootopia.lootopia.Repositories;
-
-import com.lootopia.lootopia.Entities.Step;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface StepRepository extends JpaRepository<Step, UUID> {
-}
